96cdda63086681f86ebbc9d411e2a63e601f3874
Greniert <39262221+Greniert@users.noreply.github.com>
2024-02-22 09:16:50 +0100

    ST-2 - postcss configuration

---
+++ webpack.mix.js
@@ -0,0 +1,29 @@
+let mix = require('laravel-mix');
+let path = require('path');
+
+mix.setResourceRoot('../');
+mix.setPublicPath(path.resolve('./'));
+
+mix.webpackConfig({
+    stats: {
+      children: true
+    },
+    watchOptions: { ignored: [
+        path.posix.resolve(__dirname, './node_modules'),
+        path.posix.resolve(__dirname, './css'),
+        path.posix.resolve(__dirname, './js')
+    ] }
+});
+
+mix.js('src/js/app.js', 'assets');
+
+mix.postCss("src/css/app.css", "assets",[
+    require('postcss-custom-properties')
+]);
+
+
+if (mix.inProduction()) {
+    mix.version();
+} else {
+    mix.options({ manifest: false });
+}
